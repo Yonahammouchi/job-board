1b8528aa6e8b431b041a4df98ef7e7e27d031fa2
Yona <yona.hammouchi@outlook.com>
2024-08-29 21:15:02 +0200

    Routing, Login page in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Register from './pages/Register';
 
 const App = () => {
   return (
-<BrowserRouter>
-<Routes>
-  <Route path='/' element={<Login/>}/>
-  <Route path='/404' element={<NotFound/>}/>
-</Routes>
-</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/*' element={<NotFound />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleNavigate = () => {
+        navigate('/register')
+    }
+
+    const handleCheckLogin = () => {
+        alert("USERNAME : " + username + " and PASSWORD : " + password)
+    }
+
     return (
-        <div>
-            page login
-        </div>
+        <>
+            <div className='center_login'>
+                <div className='flex_login'>
+
+                    <h1>Page de connexion</h1>
+                    <p>Veuillez vous connecter</p>
+                    <input placeholder='username' onChange={(event) => setUsername(event.target.value)}></input>
+                    <input placeholder='password' type='password' onChange={(event) => setPassword(event.target.value)}></input>
+                    <button onClick={handleCheckLogin}>Se connecter</button>
+                    <button onClick={handleNavigate}>S'enregistrer</button>
+
+                </div>
+            </div>
+        </>
     );
 };
 
